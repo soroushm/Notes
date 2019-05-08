92771f8ffcc7680d87305391e9d065ac23b52f45
masoud <masoud.s.geo@gmail.com>
2019-05-08 20:51:17 +0430

    scss is not good enough for react 😠 dive into jss 💑

--- src/App.js
+++ src/App.js
@@ -3,15 +3,36 @@
 import React from 'react'
 import { Provider } from 'react-redux'
 import { Router } from 'react-router-dom'
+import { ThemeProvider } from 'react-jss'
 import { store, history } from 'src/store'
 import Routes from 'src/Routes'
 import 'src/App.css'
+
+export const theme = {
+  direction: 'ltr',
+  palette: {
+    primary: {
+      light: 'red',
+      main: 'red',
+      dark: 'red'
+    },
+    secondary: {
+      light: 'blue',
+      main: 'blue',
+      dark: 'blue'
+    },
+    error: 'red',
+    text: 'gray'
+  }
+}
 
 function App() {
   return (
     <Provider store={store}>
       <Router history={history}>
-        <Routes />
+        <ThemeProvider theme={theme}>
+          <Routes />
+        </ThemeProvider>
       </Router>
     </Provider>
   )
