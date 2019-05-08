edb7e4875c6c9fe1aa08a2038120082028178c6e
masoud <masoud.s.geo@gmail.com>
2019-05-08 20:52:56 +0430

    Grid is come in 🎂

---
+++ src/common/elements/Grid.js
@@ -0,0 +1,45 @@
+// @flow
+
+// ************* Note *************
+// Gide for Flex Style https://css-tricks.com/snippets/css/a-guide-to-flexbox/
+
+// ************* WTF  *************
+// spend one hour on this Grid we should be use http://material-ui.com
+
+import React from 'react'
+import injectSheet from 'react-jss'
+import humps from 'humps'
+
+type Props = {
+  classes: Object,
+  children?: any,
+  container?: boolean,
+  direction?: 'row' | 'row-reverse' | 'column' | 'column-reverse',
+  alignItems?: 'flex-start' | 'center' | 'flex-end' | 'stretch' | 'baseline',
+  alignSelf?: 'auto' | 'flex-start' | 'flex-end' | 'center' | 'baseline' | 'stretch',
+  alignContent?: 'stretch' | 'center' | 'flex-start' | 'flex-end' | 'space-between' | 'space-around',
+  justify?: 'flex-start' | 'center' | 'flex-end' | 'space-between' | 'space-around' | 'space-evenly',
+  styles?: Object
+}
+
+const Grid = (props: Props) => {
+  console.log('prr', props)
+  return <div className={props.classes.root}>{props.children}</div>
+}
+
+const styles = () => ({
+  root: ({ container, direction, alignItems, alignContent, alignSelf, justify, styles = {} }) => {
+    const { root = null } = styles
+    return {
+      display: container && 'flex',
+      direction: direction && humps.camelizeKeys(direction),
+      alignItems: alignItems && humps.camelizeKeys(alignItems),
+      alignContent: alignContent && humps.camelizeKeys(alignContent),
+      alignSelf: alignSelf && humps.camelizeKeys(alignSelf),
+      justifyContent: justify && humps.camelizeKeys(justify),
+      ...root
+    }
+  }
+})
+
+export default injectSheet(styles)(Grid)
--- src/user/componnets/Page.js
+++ src/user/componnets/Page.js
@@ -1,4 +1,5 @@
 import React, { useEffect } from 'react'
+import Grid from 'src/common/elements/Grid'
 import '../styles/page.scss'
 
 const Page = (props) => {
@@ -7,21 +8,29 @@ const Page = (props) => {
   })
   return (
     <div className="container">
-      <form
-        onSubmit={(e) => {
-          e.preventDefault()
-          console.log('submit')
-        }}
+      <Grid
+        container
+        alignContent="flex-start"
+        justify="center"
+        alignItems="center"
+        styles={{ root: { height: '100vh' } }}
       >
-        <input
-          onChange={(val) => {
-            console.log('ccc', val)
+        <form
+          onSubmit={(e) => {
+            e.preventDefault()
+            console.log('submit')
           }}
-        />
-        <button type="submit" name="submit">
-          submit
-        </button>
-      </form>
+        >
+          <input
+            onChange={(val) => {
+              console.log('ccc', val)
+            }}
+          />
+          <button type="submit" name="submit">
+            submit
+          </button>
+        </form>
+      </Grid>
     </div>
   )
 }
